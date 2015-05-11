0a6fad65af9e6ec4273ab9d6230c6cbf802e917d
Lisa <lisakbuch@gmail.com>
2015-05-11 19:26:09 -0400

    server updated

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+
+
 var db = require('./config/db');
 var config = require('./config/config');
 
+var express = require("express");
+var mongoose = requir('mongoose');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+mongoose.contect('mongodb://localhost/vegApp');
+mongoose.connection.once('open', function(){
+	console.log('connected');
+});
+
+var app = express();
+app.locals.pretty = true;
+
+app.use(bodyParser.json());
+console.log(__dirname);
+console.log(path.normalize(__dirname));
+var root = path.normalize(__dirmane + '/..');
+console.log(root);
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(root, 'client')));
+app.set('appPath', 'client');
+
+require('./routes')(app);
+
+app.listen(9000);
